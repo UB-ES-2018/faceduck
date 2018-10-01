ec9886119fb9b4815efafabbf2f6ec517f13542a
Iván Sevilla Ramos <ivi24.s.r@gmail.com>
2018-10-01 08:47:18 +0200

    Modification of buttons to class

--- frontend/app/src/views/Homepage.vue
+++ frontend/app/src/views/Homepage.vue
@@ -1,12 +1,14 @@
 <template>
 <body>
-    <div id='title'>
+    <div class='title'>
         <h3>FACEDUCK</h3>
     </div>
 <a>
     <div id='buttons'>
-        <button id='but_sign' href="/register">Register</button>
-        <button id='but_sign' href="/login">Log in</button>
+        
+        <button class='but_sign' href="/register">Register</button> 
+
+        <button class='but_sign' href="/login">Log in</button>
     </div>
     </a>
 </body>
@@ -19,7 +21,7 @@ html
 
 
 
-#title
+.title
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
@@ -29,14 +31,15 @@ html
   font-size: 450%
 
 
-//fonts
-$font_0: sans-serif
 
-#but_sign 
+.but_sign 
+  
+ 
   font-size: 100%
+   
   display: inline-block
   padding: .938em 1.875em
-  border: .094em solid #743EA5
+  border: .094em solid #ffb451
   border-radius: .625em
   background-color: #ffb511
   box-shadow: 0 0.375em 0.313em -0.313em rgba(0, 0, 0, 0.8), inset 0 0.063em rgba(255, 255, 255, 0.4), inset 0 -0.188em rgba(0, 0, 0, 0.15)
@@ -45,11 +48,11 @@ $font_0: sans-serif
   text-transform: uppercase
   text-shadow: 0 .063em #000
   cursor: pointer
-  font: bold 1.8rem sans-serif
+  font: bold 1.8rem 'Avenir'
   &:hover
     background-color: #ffb516
-    box-shadow: 0 0.125em rgba(255, 255, 255, 0.4), inset 0 0.063em rgba(255, 255, 255, 0.4), inset 0 -0.188em #804FAD
+    box-shadow: 0 0.125em rgba(255, 255, 255, 0.4), inset 0 0.063em rgba(255, 255, 255, 0.4), inset 0 -0.188em #ffb451
   &:active
-    box-shadow: inset 0 0 1.094em #472566, inset 0 0.063em #390668, inset 0 -0.188em #682CA0, 0 0.063em rgba(255, 255, 255, 0.4)
+    box-shadow: inset 0 0 1.094em #ffb451, inset 0 0.063em #ffb351, inset 0 -0.188em #ffe451, 0 0.063em rgba(255, 255, 255, 0.4)
 </style>
 
