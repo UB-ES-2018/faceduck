c1b6067f19c1c9f6465b10d8a4299d14811495bd
maimg <maimg15@gmail.com>
2018-10-06 20:22:56 +0200

    TextPost in process
    Final touches to be done

--- frontend/app/src/views/PersonalWall.vue
+++ frontend/app/src/views/PersonalWall.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="PersonalWall">
-    <h1>This is the Wall</h1>
+    <div>
+      <h1>This is the Wall</h1>
+    </div>
+    <div class="container" align="center">
+      <PostView/>
+    </div>
   </div>
 </template>
+
+<script>
+import PostView from "../components/PostView.vue";
+
+export default {
+  name: 'PersonalWall',
+  components: {
+    PostView,
+  },
+};
+</script>
