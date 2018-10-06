f7829281d57b48eb05f6cb19a51c126d23dfac39
Vemka <bevz.test@gmail.com>
2018-10-06 10:25:10 +0200

    Added CORS
    check if everything works properly - my simple ajax tests showed that it works, but somehow ignores origins settings - for me ip whitelist didnt work

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,17 @@ from flask import Flask
 from faceduck.blueprints import all_blueprints
 from importlib import import_module
 from faceduck import config
+from flask_cors import CORS
+#import logging
 
 app = Flask(__name__)
+
+
+# CORS config
+#replace * with a list of IPs that are allowed api cconnection
+cors = CORS(app, resources={r"/*": {"origins": ['*']}})
+#app.config['CORS_HEADERS'] = 'Content-Type'
+#logging.getLogger('flask_cors').level = logging.DEBUG
 
 if __name__ == '__main__':
     for bp in all_blueprints:
