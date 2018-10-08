4fa3e0da051db7dcc1acea17f22d7f4debd7bb89
Ferran Lorenzo <lorenzo.ferran@gmail.com>
2018-10-08 22:42:48 +0200

    Error parentesis

--- backend/app/faceduck/core/signup.py
+++ backend/app/faceduck/core/signup.py
@@ -2,7 +2,7 @@ from faceduck.models.user import User
 import uuid
 
 def createUser(user, email, password, name, surname, birthday, gender):
-		newUser = User(meta='id': uuid.uuid4()},  username=user, email=email, password=password, name=name, surname=surname, birthday=birthday, gender=gender)
+		newUser = User({meta='id': uuid.uuid4()},  username=user, email=email, password=password, name=name, surname=surname, birthday=birthday, gender=gender)
 		newUser.save()
 	
 	return newUser
