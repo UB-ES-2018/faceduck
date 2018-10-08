8f9803c78431a0987de44247be2b5f021f369f51
Ferran Lorenzo <lorenzo.ferran@gmail.com>
2018-10-08 18:56:33 +0200

    Errors canviats
    Canvi en el fallo que donava al compilar, implementat uuid, podeu mirar si em compila? M'he barallat un munt d'hores amb docker i no hi ha manera...

--- backend/app/faceduck/core/signup.py
+++ backend/app/faceduck/core/signup.py
@@ -1,16 +1,16 @@
 from faceduck.models.user import User
 
-def createUser(user, email, password, name, surname, birthday, gender):
+def createUser(id, user, email, password, name, surname, birthday, gender):
 
 	newUser = None
 	
-	try{
-		User.get(username = user):
-		User.get(email=email):
+	try:
+		User.get(username = user)
+		User.get(email=email)
 	except ValueError:
 		return newUser
 	
-	newUser = User(username=user, email=email, password=password, name=name, surname=surname, birthday=birthday, gender=gender)
+	newUser = User(meta='id': id},  username=user, email=email, password=password, name=name, surname=surname, birthday=birthday, gender=gender)
 	newUser.save()
 	
 	return newUser
--- backend/app/faceduck/views/api.py
+++ backend/app/faceduck/views/api.py
@@ -1,11 +1,13 @@
 from faceduck.blueprints import api
 from faceduck.core import signup
 import json
+import uuid
 
 @api.route('/user', methods=["POST"])
 def signup():
 	req = request.get_json()
 	try:
+		id = uuid.uuid4()
 		username = req['username']
 		email = req['email']
 		password = req['password']
@@ -17,7 +19,6 @@ def signup():
 		return json.dumps({'error-id': '400', 'error-message': 'Invalid data submitted'})
 	
 	try:
-		signup.createUser(username, email, password, name, surname, birthday, gender)
+		signup.createUser(id, username, email, password, name, surname, birthday, gender)
 	except ValueError:
 		return json.dumps({'error-id': '400', 'error-message': "Username or Email already used"})
-	
