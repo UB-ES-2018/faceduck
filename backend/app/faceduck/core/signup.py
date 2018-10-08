6f8cac8a2101d9c6d4194bd8d52d10372c71ecd1
Ferran Lorenzo <lorenzo.ferran@gmail.com>
2018-10-08 22:27:38 +0200

    Nova versió
    He posat que a signup.py es intenti crear usuari, i si peta saltara el try-except de signup i retornara error, al David ja li compila

---
+++ backend/app/faceduck/core/signup.py
@@ -0,0 +1,8 @@
+from faceduck.models.user import User
+import uuid
+
+def createUser(user, email, password, name, surname, birthday, gender):
+		newUser = User(meta='id': uuid.uuid4()},  username=user, email=email, password=password, name=name, surname=surname, birthday=birthday, gender=gender)
+		newUser.save()
+	
+	return newUser
--- backend/app/faceduck/views/api.py
+++ backend/app/faceduck/views/api.py
@@ -1,5 +1,22 @@
 from faceduck.blueprints import api
+from faceduck.core import signup
+import json
 
-@api.route('/')
-def index():
-    return "hellow"
+@api.route('/user', methods=["POST"])
+def signup():
+	req = request.get_json()
+	try:
+		username = req['username']
+		email = req['email']
+		password = req['password']
+		name = req['name']
+		surname = req['surname']
+		birthday = req['birthday']
+		gender = req['gender']
+	except KeyError:
+		return json.dumps({'error-id': '400', 'error-message': 'Invalid data submitted'})
+	
+	try:
+		signup.createUser(username, email, password, name, surname, birthday, gender)
+	except ValueError:
+		return json.dumps({'error-id': '400', 'error-message': "Username or Email already used"})
