30a85b73fc38b1f2361266788e87538a80448403
Ferran Lorenzo <lorenzo.ferran@gmail.com>
2018-10-09 22:27:17 +0200

    US-02-Login API Implementation
    Primera implementació login

--- backend/app/faceduck/views/api.py
+++ backend/app/faceduck/views/api.py
@@ -4,6 +4,9 @@ from faceduck.blueprints import api
 from faceduck import core
 from .mappers import ERRORS
 from faceduck.utils import FaceduckError
+from faceduck.core import login
+import json
+from werkzeug.security import generate_password_hash
 
 
 def client_error(error_id):
@@ -34,3 +37,16 @@ def signup():
     
     return ("", 204)
 
+
+@api.route('/user', methods=["POST"])
+def login():
+    req = request.get_json()
+    try:
+        username = req['username']
+        password = req['password']
+    except KeyError:
+        return json.dumps({'error-id': '400', 'error-message': 'Invalid data submitted'})
+    try:
+        login.loginUser(username, generate_password_hash(password))
+    except ValueError:
+        return json.dumps({'error-id': '400', 'error-message': "Invalid username/email or password"})
