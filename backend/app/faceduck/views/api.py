60943a56e2c71f81174d3c84080bb54aadc810fc
David Martinez Carpena <dmc909@gmail.com>
2018-10-10 11:58:58 +0200

    Created the route controller for the create post and get post endpoints.

--- backend/app/faceduck/views/api.py
+++ backend/app/faceduck/views/api.py
@@ -1,5 +1,42 @@
+import json
+from flask import make_response, request
 from faceduck.blueprints import api
+from faceduck import core
+from flask import jsonify
+from .mappers import post_mapper, ERRORS
 
-@api.route('/')
-def index():
-    return "hellow"
+
+def client_error(error_id):
+    return make_response(jsonify(**ERRORS["001"]), 400)
+
+
+@api.route("/post", methods=["POST"])
+def create_post():
+    if not request.is_json:
+        return client_error("001")
+    
+    try:
+        text = request.json["text"]
+        author_id = request.json["author-id"]
+    except KeyError:
+        return client_error("001")
+
+    try:
+        post = core.create_post(text, author_id)
+        response = post_mapper(post)
+    except ValueError:
+        return client_error("001")
+    
+    return jsonify(response)
+
+
+@api.route("/post/<post_id>")
+def get_post(post_id):
+    try:
+        post = core.get_post(post_id)
+        response = post_mapper(post)
+    except ValueError:
+        return client_error("001")
+    
+    return jsonify(response)
+
---
+++ backend/app/faceduck/views/mappers.py
@@ -0,0 +1,39 @@
+from faceduck.core.user import create_user, get_user
+from faceduck.models.post import Post
+from faceduck.models.user import User
+
+ERRORS = {
+    "001": {"error-id": "001", "error-message": "Invalid data"},
+    "002": {"error-id": "002", "error-message": "Already existing username"},
+    "003": {"error-id": "003", "error-message": "Already existing email"},
+    "004": {"error-id": "004", "error-message": "This email or password is invalid"}
+}
+
+
+def user_mapper(user):
+    user_dict = {"id": user.meta.id}
+    
+    for attr in dir(user):
+        key = attr.replace("_", "-")
+        
+        if attr != "password":
+            user_dict[key] = getattr(user, attr)
+
+    return user_dict
+
+
+def post_mapper(post):
+    post_dict = {"id": post.meta.id}
+    
+    for attr in dir(post):
+        key = attr.replace("_", "-")
+        
+        if attr == "author":
+            user = get_user(getattr(post, attr))
+            user_dict = user_mapper(user)
+            post_dict[key] = user_dict
+        else:
+            post_dict[key] = getattr(post, attr)
+
+    return post_dict
+
