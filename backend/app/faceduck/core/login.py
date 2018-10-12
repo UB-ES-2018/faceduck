bf2d02f4a064ee870dda3a2b9d649cf8b3bc520f
Francesc Bruguera <francesc@francesc.xyz>
2018-10-12 14:33:43 +0200

    Simplify user login

---
+++ backend/app/faceduck/core/login.py
@@ -0,0 +1,32 @@
+from faceduck.models.user import User
+from faceduck.core.sessionkeeping import SessionKeeping
+from werkzeug.security import check_password_hash
+
+
+def get_user_by_email(email):
+    email_response = User.search().from_dict({
+         "query": {
+             "match_phrase": {
+                 "email": email
+             }
+         }
+     }).execute()
+
+    if len(email_response.hits) == 0:
+        return None
+
+    return User.from_es(email_response.hits[0])
+
+
+def login_user(email, password):
+    user = get_user_by_email(email)
+
+    if user is None:
+        raise FaceduckError("004")
+
+    if not check_password_hash(user.password, password):
+        raise FaceduckError("004")
+
+    token = SessionKeeping.generate_jwt_token(user.meta.id)
+
+    return user, token
--- backend/app/faceduck/core/sessionkeeping.py
+++ backend/app/faceduck/core/sessionkeeping.py
@@ -1,8 +1,15 @@
 from faceduck.models.user import User
+from flask_jwt_extended import (
+    create_access_token
+)
 
 
 class SessionKeeping:
     @staticmethod
     def identity(uid):
         return User.get(id=uid)
+
+    @staticmethod
+    def generate_jwt_token(uid):
+        return create_access_token(uid)
 
