97d6dc6185d310928466feba7b2213de8359fe3a
Enric Florit <efz1005@gmail.com>
2018-10-13 13:43:51 +0200

    Working US1 & US2 integration. Yay!

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ def user_loader_callback(identity):
 
 # CORS config
 # add needed IPs or regExes to origins to allow api cconnection
-cors = CORS(app, resources={r"/*": {"origins": ['http://127.0.0.1', 'http://localhost' ,'http://192.168.99.100']}}, supports_credentials=True)
+cors = CORS(app, resources={r"/*": {"origins": ['http://127.0.0.1:8080', 'http://localhost:8080' ,'http://192.168.99.100:8080']}}, supports_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == '__main__':
