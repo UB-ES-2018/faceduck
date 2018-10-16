377f0bb6b7c1abf41be2a1fe0b46149988b5f2fd
Enric Florit <efz1005@gmail.com>
2018-10-16 16:40:03 +0200

    Fix uploader content-type

--- frontend/app/src/components/ImageUploader.vue
+++ frontend/app/src/components/ImageUploader.vue
@@ -22,11 +22,14 @@ export default {
         handleFileUpload() {
             var formData = new FormData();
             formData.append('file', this.$refs.image.files[0]);
+            
             fetch(apiUploadUrl, {
                 method: "POST",
                 headers: {
                     "Authorization": "Bearer " + localStorage.getItem("access-token"),
-                    "Content-Type": "multipart/form-data"
+                    // If you add this, upload won't work
+                    // SRC: https://stanko.github.io/uploading-files-using-fetch-multipart-form-data/
+                    // "Content-Type": "multipart/form-data"
                 },
                 body: formData
             }).catch((r) => console.log(r))
@@ -34,7 +37,7 @@ export default {
                 if (response.ok) {
                     response.json().then((json) => {
                         this.$root.$emit("imageUpload", {
-                            emitter: this["uploader-id"],
+                            emitter: this.uploaderId,
                             url: json["media-url"]
                         });
                     });
