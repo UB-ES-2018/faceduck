5535c7715f65d5debcc1adb535d6efd91208cf1d
Enric Florit <efz1005@gmail.com>
2018-10-21 16:20:15 +0200

    Fixed the API behavior (pending: documentation)

--- backend/app/faceduck/core/__init__.py
+++ backend/app/faceduck/core/__init__.py
@@ -5,6 +5,6 @@ from .user import get_user
 from .upload import upload_media
 from .user_search import search_users
 from .post_search import search_posts
-from .friendship import create_friendship,update_friendship,get_friends,delete_friendship
+from .friendship import exists_friendship,create_friendship,update_friendship,get_friends,delete_friendship
 from .post_search import search_posts, search_posts_by_author
 
