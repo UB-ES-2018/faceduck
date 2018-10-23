c1376cb38afbd6c87d1f1319b6fc761cd20f67d7
maimg <maimg15@gmail.com>
2018-10-23 02:05:41 +0200

    navbar incluida a search page

--- frontend/app/src/views/SearchResults.vue
+++ frontend/app/src/views/SearchResults.vue
@@ -1,5 +1,13 @@
 <template>
     <div id='SearchResults'>
+      <nav class="navbar navbar-light" style="background-color: pale-sky;">
+        <h1 class="title">Faceduck</h1>
+          <form class="form-inline">
+            <div class="page mr-sm-2" v-on:click='profile' :userName="setJson()"> {{ userName }} </div>
+            <div class="page my-2 mr-sm-2" v-on:click='wall'>Wall</div>
+            <button class="button" v-on:click='logout'> Log Out </button>
+          </form>
+      </nav>
       <h1>Search results</h1>
       <SearchBar/>
       <template v-if="nores">
@@ -28,6 +36,7 @@ import SearchBar from "../components/SearchBar.vue";
 export default {
     name: "SearchResults",
     data() {
+        userName: {};
         return{
             results: [],
             nores: false,//ugly way to hide nothing found message
@@ -42,6 +51,21 @@ export default {
     },
     components: {
       SearchBar
+    },
+    methods: {
+      profile () {
+        this.$router.push("/profile");
+      },
+      wall () {
+        this.$router.push("/wall");
+      },
+      logout: function() {
+        localStorage.removeItem("access-token");
+        this.$router.push("/");
+      },
+      setJson() {
+        this.userName = JSON.parse(localStorage.getItem("user"))["username"]
+      } 
     }
 }
 </script>
@@ -69,4 +93,17 @@ h4
 .border-b-1
   border-bottom: 1px solid rgba(225,225,225,.16)
 
+.button
+  background-color: #ffb511
+  border: none
+  color: white
+  font-size: 12px
+  font-weight: bold
+  box-sizing: content-box
+  padding: 10px
+  border-radius: 10px
+  width: 60px
+  left: 40% 
+  cursor: pointer
+  
 </style>
