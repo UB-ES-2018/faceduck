c3666bbdb62e02a03dc51df749bc3bb35679bec1
David Martinez Carpena <dmc909@gmail.com>
2018-11-10 09:12:58 +0100

    New get_all_users core function that returns an iterator of all the users.

--- backend/app/faceduck/core/__init__.py
+++ backend/app/faceduck/core/__init__.py
@@ -1,7 +1,7 @@
 from .signup import create_user
 from .login import login_user
 from .post import create_post, get_post
-from .user import get_user
+from .user import get_user, get_all_users
 from .upload import upload_media
 from .user_search import search_users
 from .post_search import search_posts
