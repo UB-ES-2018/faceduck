af092d8e1fc934dfa9520bc288526219674219e8
maimg <maimg15@gmail.com>
2018-11-13 17:34:00 +0100

    contadores más o menos funcionales

--- frontend/app/src/components/EmotionCounter.vue
+++ frontend/app/src/components/EmotionCounter.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="emotions" id="emotions" v-show="isVisible"><!--&lt;EmotionButtons/&gt;-->
-        <img src="http://www.northamericangoldwings.com/community/forums/uploads/reactions/facebook-love-png-44003.png" class="react">  {{like}}
-        <img src="http://www.freeiconspng.com/uploads/facebook-live-love-png-1.png" class="react" v-on:click="addReaction('love')">  {{love}}
-        <img src="http://clipart.info/images/ccovers/1499793248facebook-haha.png" class="react" v-on:click="addReaction('laughing face')">  {{laughing}}
-        <img src="https://cdn4.iconfinder.com/data/icons/reaction/32/angry-512.png" class="react" v-on:click="addReaction('angry face')">  {{angry}}
-        <img src="http://clipart.info/images/ccovers/1499793247facebook-sad-emoji-like-png.png" class="react" v-on:click="addReaction('sad crying face')">  {{sad}}
+        <div v-if="post['reactions-count'][0]['reaction'] === 'like'"><img src="http://www.northamericangoldwings.com/community/forums/uploads/reactions/facebook-love-png-44003.png" class="react">  {{post['reactions-count'][0]['count']}}</div>
+        <div v-if="post['reactions-count'][0]['reaction'] === 'love'"><img src="http://www.freeiconspng.com/uploads/facebook-live-love-png-1.png" class="react">  {{post['reactions-count'][0]['count']}}</div>
+        <div v-if="post['reactions-count'][0]['reaction'] === 'laughing face'"><img src="http://clipart.info/images/ccovers/1499793248facebook-haha.png" class="react">  {{post['reactions-count'][0]['count']}}</div>
+        <div v-if="post['reactions-count'][0]['reaction'] === 'angry face'"><img src="https://cdn4.iconfinder.com/data/icons/reaction/32/angry-512.png" class="react">  {{post['reactions-count'][0]['count']}}</div>
+        <div v-if="post['reactions-count'][0]['reaction'] === 'sad crying face'"><img src="http://clipart.info/images/ccovers/1499793247facebook-sad-emoji-like-png.png" class="react">  {{post['reactions-count'][0]['count']}}</div>
     </div>
 </template>
 
 <script>
 export default {
 	name: "EmotionButtons",
-	props: ["post"],
+    props: ["post"],
 	data() {
 		return {
             isVisible: true,
@@ -22,19 +22,27 @@ export default {
             sad: 0,
         }
     },
-    /**
-    created() {
-        if (this.query)
-            this._query = this.query.trim();
-
-        this.$root.$on("showReaction", (event) => {
-            if (event && event.post)
-                // unshift: push to position 0
-                this.list.unshift(event.post);
-                
-        });
-    }
-    */
+    methods: {
+        update() {
+            
+            this.like = post["reactions-count"][0]["reaction"]
+            alert(this.like)
+            for (i in post["reactions-count"]){
+                alert(i["reaction"])
+                if (i["reaction"].equals("like")){
+                    this.data.like = i["count"]
+                } else if (i["reaction"].equals("love")) {
+                    this.love = i["count"]
+                } else if (i["reaction"].equals("laughing")) {
+                    this.laughing = i["count"]
+                } else if (i["reaction"].equals("angry")) {
+                    this.angry = i["count"]
+                } else if (i["reaction"].equals("sad")) {
+                    this.sad = i["count"]
+                } 
+            }
+        },
+    },
 }
 </script>
 
