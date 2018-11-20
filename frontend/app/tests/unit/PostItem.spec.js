e65306284a629dc57687daa1a72da91d80b079da
Enric Florit <efz1005@gmail.com>
2018-11-20 09:00:25 +0100

    Test hashtag links

--- frontend/app/tests/unit/PostItem.spec.js
+++ frontend/app/tests/unit/PostItem.spec.js
@@ -1,15 +1,16 @@
 import { mount } from "@vue/test-utils";
 import PostItem from "@/components/PostItem.vue";
 
+var post_mock = {
+    "author": {
+      "id": "90ada2f2-1b5d-4319-9c55-77ea566015e4",
+      "username": "john"
+    },
+    "id": "82c45212-b2cc-4699-b7cc-5ba55e7f262e", 
+    "text": "unit testing"
+};
 const factory = (values = {
-	post: {
-	    "author": {
-	      "id": "90ada2f2-1b5d-4319-9c55-77ea566015e4",
-	      "username": "john"
-	    },
-	    "id": "82c45212-b2cc-4699-b7cc-5ba55e7f262e", 
-	    "text": "unit testing"
-	}
+	post: { ...post_mock }
 }) => {
 	return mount(PostItem, {
 		propsData: { ...values }
@@ -34,5 +35,15 @@ describe("PostItem.vue", () => {
 		const wrapper = factory();
 
 		expect(wrapper.find(".post-username a").attributes("href")).toEqual("/profile/john");
-	})
+	});
+
+	it("renders links to hashtags", () => {
+		var post_tag = { ...post_mock };
+		post_tag["text"] = "post with a #hashtag to test";
+		const wrapper = factory({post: post_tag});
+
+		expect(wrapper.find(".post-text a").exists()).toBeTruthy();
+		expect(wrapper.find(".post-text a").text()).toEqual("#hashtag");
+		expect(wrapper.find(".post-text a").attributes("href")).toEqual("/search?query=%23hashtag");
+	});
 });
