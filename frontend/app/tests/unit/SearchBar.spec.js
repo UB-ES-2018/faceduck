7eb600d93487a0a0d50b1007ce22007077966a46
Iván Sevilla Ramos <ivi24.s.r@gmail.com>
2018-11-27 17:17:01 +0100

    Removed unfinished tests

--- frontend/app/tests/unit/SearchBar.spec.js
+++
@@ -1,8 +0,0 @@
-import { mount } from "@vue/test-utils";
-import SearchBar from "@/components/SearchBar.vue";
-
-describe('SearchBar', () => {
-    it('has a created hook', () => {
-        expect(typeof SearchBar.created).toBe('function');
-    });
-})
