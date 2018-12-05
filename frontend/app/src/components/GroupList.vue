ecb9dbe7bcbf9b6d991e2554f5337d4f0af898a2
Enric Florit <efz1005@gmail.com>
2018-12-05 10:59:31 +0100

    Created group list

---
+++ frontend/app/src/components/GroupList.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="group-list">
+		<ul class="list-group"><!-- actual bootstrap class 😅 -->
+			<li class="list-group-item"
+			 	v-for="group in groups" v-bind:key="group.id">
+				<a v-bind:href="'/group/' + group.id">
+					{{group.name}}
+				</a>
+			</li>
+			<li class="list-group-item" v-if="groups.length == 0">
+				No groups yet!
+			</li>
+		</ul>
+
+	</div>
+</template>
+
+<script>
+var host = window.location.hostname
+var apiGetUser  = '//'+ host +':5000/user/';
+
+export default {
+	name: "GroupList",
+	props: ["userId"],
+	data() {
+		return {
+			groups: [{
+				id:"1324",
+				name:"Er fúrbo"
+			},
+			{
+				id:"1325",
+				name:"TeaTime ☕️☕️"
+			},
+			{
+				id:"1326",
+				name:"Grupo del Cole"
+			}]
+		}
+	},
+	created() {
+		this.fetchGroups();
+	},
+	methods: {
+		fetchGroups() {
+			var user_id = this.userId;
+			if (user_id === undefined) {
+				user_id = JSON.parse(localStorage.getItem("user")).id;
+			}
+
+			fetch(apiGetUser + user_id, {
+				method: "GET",
+              "Authorization": "Bearer " + localStorage.getItem("access-token"),
+			}).then(res => res.json())
+			.then(user => {
+				if (user.groups !== undefined)
+					this.groups = user.groups;
+				else
+					this.groups = [];
+			});
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
